fb20b1066e5fe9edcc2b35d2f8c4e3b2fd8d70cb
Trinie J <jxsn00ks@gmail.com>
2020-07-13 10:47:04 -0400

    basics completed

--- main.js
+++ main.js
@@ -18,11 +18,16 @@ class Traveler {
         this.isHealthy = true
     }
     hunt(){
-        //increase food by 2
+        this.food === 2
     }
     eat(){
-        //consume 1 unit of food
-        //no food means isHealthy false
+        if (this.food <= 0){
+            return this.isHealthy === false
+        }
+        else{
+            this.food += 1
+        }
+        
     }
 }
 class Wagon {
@@ -31,15 +36,33 @@ class Wagon {
         this.passengersList = []
     }
     getAvailableSeatCount(){
+       return (this.capacity - this.passengersList.length)
         /*Returns the number of empty seats, determined by the capacity set when the wagon was created, subtracted by the number of passengers currently on board.*/
     }
-    join(traverler){
+    join(traveler){
+        if (this.getAvailableSeatCount <=0){
+            return 'At Max Capacity'
+        }
+        else{
+            this.passengersList.push [traveler]
+        }
         /*Adds the traveler to the wagon if there is space. If the wagon is already at maximum capacity, don't add them.*/
     }
     shouldQuarantine(){
+        let unhealthy = this.passengersList.some (traveler => traveler.isHealthy === false)
+        if (unhealthy === false){
+            return 'true'
+        }
+        else{
+            return 'false'
+        }
         /*Returns true if there is at least one unhealthy person in the wagon. Return false if not.*/
     }
     totalFood (){
+        let amountFood = 0
+        for (let index = 0; index < this.passengersList.length; index += 1){
+            amountFood += this.passengersList[index].food
+        }
         //Returns the total amount of food among all passengers in the wagon.
     }
 }
